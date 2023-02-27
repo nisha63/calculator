565c1758fbdcfbeaa90452670c6c785d47daffbd
nisha63 <123585499+nisha63@users.noreply.github.com>
2023-02-28 00:16:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// this function clear all the values
+
+function clearScreen() {
+	document.getElementById("result").value = "";
+}
+
+//this function display values
+
+function display(value){
+	document.getElementById("result").value += value;
+	
+}
+//this function evaluates the expression and returns result
+
+function calculate ()  {
+
+	var p = document.getElementById("result").value;
+	var q = eval(p);
+	document.getElementById("result").value = q;
+
+}
